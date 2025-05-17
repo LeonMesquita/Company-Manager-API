b2d804405637df13cd3d389418ea82dcffd86efd
LeonMesquita <leonmesquita16@gmail.com>
2025-05-17 16:12:45 -0300

    feat: add validations on create company

--- src/main/java/com/company_manager/api/repositories/CompanyRepository.java
+++ src/main/java/com/company_manager/api/repositories/CompanyRepository.java
@@ -1,5 +1,6 @@
 package com.company_manager.api.repositories;
 
+import com.company_manager.api.models.AddressModel;
 import com.company_manager.api.models.CompanyModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,6 +13,7 @@ public interface CompanyRepository extends JpaRepository<CompanyModel, Long> {
     boolean existsByCnpj(String cnpj);
     boolean existsByCompanyName(String cnpj);
     boolean existsByFantasyName(String cnpj);
+    boolean existsByAddress(AddressModel addressModel);
 
     CompanyModel findByCnpj(String cnpj);
     CompanyModel findByCompanyName(String companyName);
--- src/main/java/com/company_manager/api/services/CompanyService.java
+++ src/main/java/com/company_manager/api/services/CompanyService.java
@@ -1,9 +1,11 @@
 package com.company_manager.api.services;
 
 import com.company_manager.api.dtos.CompanyDTO;
+import com.company_manager.api.exceptions.GenericConflictException;
 import com.company_manager.api.models.AddressModel;
 import com.company_manager.api.models.CompanyModel;
 import com.company_manager.api.repositories.CompanyRepository;
+import jakarta.persistence.EntityExistsException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,7 +25,23 @@ public class CompanyService {
     }
 
     public CompanyModel save(CompanyDTO dto) {
+        boolean existsByCnpj = companyRepository.existsByCnpj(dto.getCnpj());
+        if (existsByCnpj)
+            throw new GenericConflictException("Já existe uma empresa com esse CNPJ!");
+
+        boolean existsByCompanyName = companyRepository.existsByCompanyName(dto.getCompanyName());
+        if (existsByCompanyName)
+            throw new GenericConflictException("Já existe uma empresa com esse nome!");
+
+        boolean existsByFantasyName = companyRepository.existsByFantasyName(dto.getFantasyName());
+        if (existsByFantasyName)
+            throw new GenericConflictException("Já existe uma empresa com esse nome fantasia!");
+
         AddressModel address = addressService.findById(dto.getAddressId());
+
+        boolean existsByAddress = companyRepository.existsByAddress(address);
+        if (existsByAddress)
+            throw new GenericConflictException("Já existe uma empresa com esse endereço!");
         CompanyModel companyModel = new CompanyModel();
         BeanUtils.copyProperties(dto, companyModel);
         companyModel.setAddress(address);
